94727a58af33ed33f84b80fda96302f31d88c7cc
emtkmkk <65014664+emtkmkk@users.noreply.github.com>
2023-05-09 23:26:42 +0900

    共有先のサーバー名を設定する機能を追加

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
-const result = window.confirm('Twitter へのシェアリンクを確認しました。この内容を Misskey.io にシェアしますか？(拡張機能 Twishare to io より)')
+chrome.storage.sync.get("instance_name", function (items) {
+	const instance_name = items.instance_name || "Misskey.io";
 
-if (result) {
-	let tw_url = new URL(window.location.href);
-	let params = tw_url.searchParams;
-	let text;
-	let url;
-	let hashtags;
-	let share_text;
-	let share_url
+	const result = window.confirm(
+		`Twitter へのシェアリンクを確認しました。この内容を ${instance_name} にシェアしますか？(拡張機能 Twishare to io より)`
+	);
 
-	if ( params.has('text') ) {
-		text = params.get('text');
-	}
-	if ( params.has('url') ) {
-		url = params.get('url');
-	}
-	if ( params.has('hashtags') ) {
-		hashtags = params.get('hashtags');
-	}
+	if (result) {
+		let tw_url = new URL(window.location.href);
+		let params = tw_url.searchParams;
+		let text;
+		let url;
+		let hashtags;
+		let share_text;
+		let share_url;
 
-	let io_url = new URL("https://misskey.io/share");
-
-	if (text){
-		if (hashtags){
-			tagged_hashtags = '#' + hashtags.replace(/\,/g, ' #');
-			share_text = text + '\n' + tagged_hashtags;
-		}else{
-			share_text = text;
+		if (params.has("text")) {
+			text = params.get("text");
 		}
-	}else if (hashtags){
-		tagged_hashtags = ' #' + hashtags.replace(/\,/g, ' #');
-		share_text = encodeURIComponent(tagged_hashtags);
-	}
-	io_url.searchParams.set('text',share_text);
-	
-	if (url) {
-		share_url = url;
-		io_url.searchParams.set('url',share_url);
-	}
-	
+		if (params.has("url")) {
+			url = params.get("url");
+		}
+		if (params.has("hashtags")) {
+			hashtags = params.get("hashtags");
+		}
 
-	location.href = io_url;
-}
+		let instance_url = new URL(`https://${instance_name}/share`);
+
+		if (text) {
+			if (hashtags) {
+				tagged_hashtags = "#" + hashtags.replace(/\,/g, " #");
+				share_text = text + "\n" + tagged_hashtags;
+			} else {
+				share_text = text;
+			}
+		} else if (hashtags) {
+			tagged_hashtags = " #" + hashtags.replace(/\,/g, " #");
+			share_text = encodeURIComponent(tagged_hashtags);
+		}
+		instance_url.searchParams.set("text", share_text);
+
+		if (url) {
+			share_url = url;
+			instance_url.searchParams.set("url", share_url);
+		}
+
+		location.href = instance_url;
+	}
+});
