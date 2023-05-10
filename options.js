ff1c3585042b68aa3dca99cf9de17bd7ebc421fe
alpaca-honke <blacktea1220@hotmail.com>
2023-05-10 10:07:20 +0900

    Update: 設定画面のUXの向上

--- options.js
+++ options.js
@@ -7,18 +7,22 @@ function Save() {
             instance_name: instance_name
                 .replace(/^https?:\/\//, "")
                 .replace(/\/(.*)$/, ""),
-        },
-        function () {}
-    );
+		}
+	).then(Load());
 }
 
 function Load() {
-    chrome.storage.sync.get("instance_name", function (items) {
+	chrome.storage.sync.get("instance_name").then((items) => {
         document.getElementById("instance_name").value =
             items.instance_name || "Misskey.io";
     });
 }
 
 document.addEventListener("DOMContentLoaded", Load);
-
+document.getElementById("instance_name").onkeydown = (e) => {
+	if(e.key == "Enter"){
+		Save();
+		document.getElementById("save_status").innerHTML = "保存されました！"
+	}
+};
 document.getElementById("save_button").addEventListener("click", Save);
