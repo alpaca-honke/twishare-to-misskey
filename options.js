21dca3342a1082cf22945a55dd82a2486e83ccbb
Alkappa/alpaca-honke <akp@ymail.ne.jp>
2023-05-11 12:46:43 +0900

    Fix: chromeネームスペースをbrowserに変更

--- options.js
+++ options.js
@@ -2,7 +2,7 @@ function Save() {
     const instance_name =
         document.getElementById("instance_name").value || "Misskey.io";
     // 文頭のhttps://と/が出た以降から文末までの文字 がある場合、その文字列を無視して保存する
-    chrome.storage.sync.set(
+    browser.storage.sync.set(
         {
             instance_name: instance_name
                 .replace(/^https?:\/\//, "")
@@ -12,7 +12,7 @@ function Save() {
 }
 
 function Load() {
-	chrome.storage.sync.get("instance_name").then((items) => {
+	browser.storage.sync.get("instance_name").then((items) => {
         document.getElementById("instance_name").value =
             items.instance_name || "Misskey.io";
     });
