74dcbf38d0c12cf63948cd842e813bf3f8fc8644
Alkappa/alpaca-honke <akp@ymail.ne.jp>
2023-05-22 10:03:27 +0900

    シェア用のフローティングボタンの設置

--- allpages.js
+++ allpages.js
@@ -3,5 +3,7 @@ const button = document.createElement('button');
 const share_img = document.createElement('img');
 button.id = '_twishare_to_misskey_share';
 button.onclick = '_TwishareToMisskeyShare';
-button.appendChild(share_img)
+share_img.src = chrome.runtime.getURL('assets/share.png');
+share_img.id = '_twishare_to_misskey_share_img';
+button.appendChild(share_img);
 body.appendChild(button);
