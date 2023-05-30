092ef4225bedcc3a8e613e5d3fcc223c5d9c4871
Alkappa/alpaca-honke <akp@ymail.ne.jp>
2023-05-30 13:55:41 +0900

    特定のサイトでシェアボタンを非表示にする設定を追加

--- js_and_css/allpages.js
+++ js_and_css/allpages.js
@@ -1,27 +1,35 @@
-if (whetherSetButton()) {
-	setButton();
+setButtonIfNeeded();
+
+//以下全部関数定義（処理は以上一行）
+
+async function setButtonIfNeeded() {
+	const whether_set_button = await whetherSetButton();
+	if (whether_set_button) {
+		setButton();
+	}
 }
 
-function whetherSetButton() {
-		// sites_to_hide_buttonはisSiteToHideButtonで読むのでここでは読まない
-	chrome.storage.sync.get(["button_visibility_on_misskey", "button_visibility"]).then((items) => {
-		if (isSiteToHideButton()) {
+async function whetherSetButton() {
+	const is_site_to_hide_button = await isSiteToHideButton();
+	if (is_site_to_hide_button) {
+		return false;
+	}
+	// sites_to_hide_buttonはisSiteToHideButtonで読むのでここでは読まない
+	const items = await chrome.storage.sync.get(["button_visibility_on_misskey", "button_visibility"]);
+	if (items.button_visibility_on_misskey === false) {
+		// Misskey上でボタンを表示させない設定のとき、Misskeyにアクセスしてたらfalse
+		const is_misskey = await isMisskey();
+		if (is_misskey) {
 			return false;
 		}
-		if (items.button_visibility_on_misskey === false) {
-			// Misskey上でボタンを表示させない設定のとき、Misskeyにアクセスしてたらfalse
-			if (isMisskey()) {
-				return false;
-			}
-		}
-		if (items.button_visibility === false) {
-			return false;
-		}
-		return true;
-	});
+	}
+	if (items.button_visibility === false) {
+		return false;
+	}
+	return true;
 }
 
-function isMisskey() {
+async function isMisskey() {
 	// MisskeyやCalckeyでは、metaタグのname="application-name"にcontent="Misskey"とか"Calckey"がついてる
 	let metatags = document.getElementsByTagName('meta');
 	for (let i = 0; i < metatags.length; i++){
@@ -35,43 +43,43 @@ function isMisskey() {
 	}
 	return false;
 }
-function isSiteToHideButton() {
-	chrome.storage.sync.get("sites_to_hide_button").then((items) => {
+async function isSiteToHideButton() {
+	const items = await chrome.storage.sync.get("sites_to_hide_button");
+	if (items.sites_to_hide_button) {
 		// 区切りのスペースごと保存してあるので展開
 		let sites = items.sites_to_hide_button.split(' ');
-		sites.some((value) => {
+		return sites.some((value) => {
 			return value
-				// 文頭のhttps://と/が出た以降から文末までの文字 がある場合、その文字列を無視して保存する
-                .replace(/^https?:\/\//, "")
-                .replace(/\/(.*)$/, "")
+				// 文頭のhttps://と/が出た以降から文末までの文字 がある場合、その文字列を無視
+				.replace(/^https?:\/\//, "")
+				.replace(/\/(.*)$/, "")
 				=== location.hostname;
 		});
-	});
+	}
+	return false;
 }
 
-function setButton(button_visibility){
-	if (button_visibility !== false){
-		const body = document.body;
-		const button = document.createElement('button');
-		const share_img = document.createElement('img');
-		button.id = '_twishare_to_misskey_share';
-		share_img.src = chrome.runtime.getURL('assets/share.png');
-		share_img.id = '_twishare_to_misskey_share_img';
-		button.appendChild(share_img);
-		body.appendChild(button);
-		button.addEventListener('click', () => {
-			chrome.storage.sync.get("instance_name").then((items) => {
-				const instance_name = items.instance_name || "misskey.io";
-				const now_url = location.href;
-				const now_title = document.title;
+function setButton(){
+	const body = document.body;
+	const button = document.createElement('button');
+	const share_img = document.createElement('img');
+	button.id = '_twishare_to_misskey_share';
+	share_img.src = chrome.runtime.getURL('assets/share.png');
+	share_img.id = '_twishare_to_misskey_share_img';
+	button.appendChild(share_img);
+	body.appendChild(button);
+	button.addEventListener('click', () => {
+		chrome.storage.sync.get("instance_name").then((items) => {
+			const instance_name = items.instance_name || "misskey.io";
+			const now_url = location.href;
+			const now_title = document.title;
 
-				const instance_url = new URL(`https://${instance_name}/share`);
-				if (now_title){
-					instance_url.searchParams.set("text", now_title);
-				}
-				instance_url.searchParams.set("url", now_url);
-				location.href = instance_url.href;
-			});
+			const instance_url = new URL(`https://${instance_name}/share`);
+			if (now_title){
+				instance_url.searchParams.set("text", now_title);
+			}
+			instance_url.searchParams.set("url", now_url);
+			location.href = instance_url.href;
 		});
-	}
+	});
 }
